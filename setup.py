f57d36ab81f6e39a7b7b59576bd7621934fb62ca
Guillermo <jdkdejava@gmail.com>
2016-09-09 10:53:01 +0200

    Creacion del setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import sys
+from distutils.core import setup
+
+kwargs = {}
+if 'py2exe' in sys.argv:
+    import py2exe
+    kwargs = {
+        'console' : [{
+            'script'         : 'init.py',
+            'description'    : 'Programa base para leer y enviar informacion.',
+            #'icon_resources' : [(0, 'icon.ico')]
+            }],
+        'zipfile' : None,
+        'options' : { 'py2exe' : {
+            #'dll_excludes'   : ['w9xpopen.exe'],
+            'bundle_files'   : 1,
+            'compressed'     : True,
+            'optimize'       : 2
+            }},
+         }
+
+setup(
+    name='RFExplorerClient',
+    author='Guillermo Lopez Garcia',
+    author_email='guillermolopezgarcia96@gmail.com',
+    **kwargs
+    )
